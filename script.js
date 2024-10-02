cbe6ec6131ceeb1cdafc1ed1e324b5b8abac9f32
FajMan <fajmantechnologies@gmail.com>
2024-10-02 13:31:12 +0100

    second commit

--- script.js
+++ script.js
@@ -185,3 +185,16 @@ async function display() {
 }
 
 display();
+
+
+const fetchApi = async () => {
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch", error);
+  }
+};
+
+fetchApi().then((result) => console.log(result));
